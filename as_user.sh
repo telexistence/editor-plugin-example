883f9bc29203c0cd8641de1dfdf2473717b54b33
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-04-23 16:28:40 +0900

    Add gitconfig instruction

--- as_user.sh
+++ as_user.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $HOME/.config/tmux
 ln -s $PWD/tmux.conf $HOME/.config/tmux/tmux.conf
+ln -s $PWD/gitconfig $HOME/.gitconfig
 
 if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
