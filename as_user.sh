ba7057b772ee2d0970d7931e15ab1e6365ec2898
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-04-23 15:59:34 +0900

    Update installation and add README

--- as_user.sh
+++ as_user.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p $HOME/.config/tmux
+ln -s $PWD/tmux.conf $HOME/.config/tmux/tmux.conf
+
 if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
     yes | $HOME/.fzf/install
