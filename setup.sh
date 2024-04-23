95b9d203405771f18445575f52798ebd129d36a6
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-04-23 15:43:35 +0900

    Update scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-    yes | $HOME/.fzf/install
-fi
+# Run as root
+apt update
+apt install git
+# Please clean cache after using apt
+apt-get clean \
+rm -rf /var/lib/apt/lists/*
+
+
+# 2. If you want run script as user, you can do like this
+su $USER -c "./as_user.sh"
+
